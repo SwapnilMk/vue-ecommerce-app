67a05e6f0fd4ac679aa2938ad9842d95d6276a36
SwapnilMk <mswapnil218@gmail.com>
2025-11-13 11:43:01 +0530

    cart and wishlist routes added

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -4,10 +4,14 @@ const userRoutes = require("./user.routes.js");
 const productRoutes = require("./product.routes.js");
 const orderRoutes = require("./order.routes.js");
 const dashboardRoutes = require("./dashboard.routes.js");
+const cartRoutes = require("./cart.routes.js");
+const wishlistRoutes = require("./wishlist.routes.js");
 
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
 router.use("/orders", orderRoutes);
 router.use("/dashboard", dashboardRoutes);
+router.use("/cart", cartRoutes);
+router.use("/wishlist", wishlistRoutes);
 
 module.exports = router;
